b79f1050f71ffb6df1e496b873909f20725ef01b
tong12580 <yutong@rrjc.com>
2018-03-25 18:17:34 +0800

    Jwt优化

---
+++ common-lib/src/main/java/com/jokers/pojo/bo/JwtBo.java
@@ -0,0 +1,82 @@
+package com.jokers.pojo.bo;
+
+import java.util.Date;
+
+/**
+ * UserBo
+ * Created by yuTong on 2018/03/08.
+ */
+public class JwtBo {
+    private String username;
+    private Long userId;
+    private String roleName;
+    private String subject;
+    private String secret;
+    private String issuer;
+    private Date expiresAt;//到期时间
+    private Date issuedAt;//签发时间
+
+    public Date getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(Date issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
