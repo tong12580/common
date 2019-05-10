fb7967537389678706419390404fc770c74cb974
tong12580 <467942250@hotmail.com>
2019-05-10 22:17:34 +0800

    对象赋值

---
+++ common-lib/src/main/java/com/jokers/common/other/serializer/BeanUtil.java
@@ -0,0 +1,43 @@
+package com.jokers.common.other.serializer;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.lang.reflect.Field;
+
+/**
+ * <p>BeanUtil</p>
+ * <span>豆子转换</span>
+ *
+ * @author yuTong
+ * @version 1.0
+ * @since 2019/5/10 21:30
+ */
+public class BeanUtil<T> {
+
+
+    /**
+     * 将新对象的有的新值的属性赋值给旧对象
+     *
+     * @param oldBean 老对象
+     * @param newBean 新对象
+     * @return T
+     */
+    public T copyProperties(T oldBean, T newBean) {
+        Field[] fields = oldBean.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                String name = field.getName();
+                Field newFiled = newBean.getClass().getDeclaredField(name);
+                newFiled.setAccessible(true);
+                field.setAccessible(true);
+                if (null == newFiled.get(newBean) || StringUtils.isBlank(newFiled.get(newBean).toString())) {
+                    continue;
+                }
+                field.set(oldBean, newFiled.get(newBean));
+            }
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return oldBean;
+    }
+}
